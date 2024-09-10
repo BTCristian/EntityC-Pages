92002c35088c38adecdc53939229455a854264cd
Tiberiu Badea <tcristian.b@gmail.com>
2024-09-10 11:43:53 +0100

    Issue #1:[WIP]: Build with Vite, JS, TailwindCSS, config and install packages, add content to readme

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex flex-col items-center text-center text-3xl font-bold ">
+        EntityC Pages
+      </h1>
+    </>
+  );
+}
+
+export default App;
